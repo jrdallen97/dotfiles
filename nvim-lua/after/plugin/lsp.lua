47a4c5c1ceacc44a0f7738252fee6817e81d9b60
James Allen <24575899+jrdallen97@users.noreply.github.com>
2023-01-03 09:15:01 +0000

    nvim: install some language servers, add some keybinds

--- nvim-lua/after/plugin/lsp.lua
+++ nvim-lua/after/plugin/lsp.lua
@@ -1,4 +1,12 @@
 local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
+
+lsp.ensure_installed({
+  'sumneko_lua',
+  'vimls',
+  'bashls',
+  'gopls'
+})
+
 lsp.setup()
--- nvim-lua/lua/jamesa/remap.lua
+++ nvim-lua/lua/jamesa/remap.lua
@@ -2,3 +2,13 @@ vim.g.mapleader = ' '
 vim.keymap.set('n', ',', '<leader>', { remap = true }) -- Use ',' as an additional leader key
 
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex) -- :Ex - show current directory
+
+-- Run files
+vim.keymap.set('n', '<leader>rr', ':w<cr>:!%:p<cr>', { desc = '[r]un current file (shebang)' })
+vim.keymap.set('n', '<leader>rt', ':w<cr>:!time %:p<cr>', { desc = '[r]un & [t]ime current file (shebang)' })
+
+-- Terminal mode
+vim.keymap.set('t', '<leader><esc>', '<c-\\><c-n>', { desc = 'escape from terminal' })
+
+-- Jump to vim settings
+vim.keymap.set('n', '<leader>ev', ':tabe ~/.config/nvim/lua/jamesa<cr>', { desc = '[e]dit [v]im settings' })
