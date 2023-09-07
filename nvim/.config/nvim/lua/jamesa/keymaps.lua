6e5c283591bed4bf2aae926a241f1342a0809ad3
James Allen <24575899+jrdallen97@users.noreply.github.com>
2023-09-07 16:13:58 +0100

    nvim: Begin migrating to lazy.nvim
    Resetting most config to start from scratch. Also using GNU stow for installation now.

---
+++ nvim/.config/nvim/lua/jamesa/keymaps.lua
@@ -0,0 +1,15 @@
+-- Quick settings toggles
+vim.keymap.set('n', '<leader>l', ':set list!<cr>', { desc = 'toggle [l]ist' })
+vim.keymap.set('n', '<leader>w', ':set wrap!<cr>', { desc = 'toggle [w]rap' })
+
+-- Move lines up & down easily
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv", { desc = 'move selection down' }) -- Move selection down, reindent & reselect
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv", { desc = 'move selection up' })
+vim.keymap.set('v', '<S-Down>', ":m '>+1<CR>gv", { desc = 'move selection down' })
+vim.keymap.set('v', '<S-Up>',   ":m '<-2<CR>gv", { desc = 'move selection up' })
+
+-- Reselect after changing indentation
+vim.keymap.set('v', '>', ">gv", { desc = 'increase indentation & reselect' })
+vim.keymap.set('v', '<', "<gv", { desc = 'decrease indentation & reselect' })
+vim.keymap.set('v', '<S-Right>', ">gv", { desc = 'increase indentation & reselect' })
+vim.keymap.set('v', '<S-Left>',  "<gv", { desc = 'decrease indentation & reselect' })
---
+++ nvim/.config/nvim/lua/plugins/core.lua
@@ -0,0 +1,31 @@
+return {
+  { 'tpope/vim-vinegar' }, -- Improvements to netrw
+  {
+    'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+    config = function()
+      -- Disable sleuth for Go - it'll always be tabs!
+      vim.g.sleuth_go_heuristics = 0
+    end,
+  },
+  {
+    'catppuccin/nvim', -- Colorscheme
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      require('catppuccin').setup({ flavour = 'macchiato' })
+      vim.cmd.colorscheme('catppuccin')
+    end,
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        highlight = {
+          enable = true
+        }
+      })
+    end
+  },
+  { 'lewis6991/gitsigns.nvim' }, -- Git changed lines in gutter
+}
