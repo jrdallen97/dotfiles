97a0dfdcccf1b9d892ee9354a1a97d9664c34c29
James Allen <24575899+jrdallen97@users.noreply.github.com>
2024-03-07 14:59:29 +0000

    Tweak mini.nvim modules

--- nvim/.config/nvim/lua/jam/plugins/core.lua
+++ nvim/.config/nvim/lua/jam/plugins/core.lua
@@ -134,24 +134,34 @@ return {
 
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      -- - ysiw) - [Y]Add [S]urround [I]nner [W]ord [)]Paren
+      -- - ysa'" - [Y]Add [S]urround [A]round [']quotes ["]quotes
+      -- - ds'   - [D]elete [S]urround [']quotes
+      -- - cs)'  - [C]hange [S]urround [)] [']
+      require('mini.surround').setup {
+        -- Override mappings to be more like tpope/vim-surround
+        -- The defaults conflict with the standard vim `s` (remove char & enter insert mode)
+        mappings = {
+          add = 'ys', -- Add surrounding in Normal and Visual modes
+          delete = 'ds', -- Delete surrounding
+          find = '', -- Find surrounding (to the right)
+          find_left = '', -- Find surrounding (to the left)
+          highlight = '', -- Highlight surrounding
+          replace = 'cs', -- Replace surrounding
+          update_n_lines = '', -- Update `n_lines`
+
+          suffix_last = '', -- Suffix to search with "prev" method
+          suffix_next = '', -- Suffix to search with "next" method
+        },
+      }
+
+      -- Minimal and fast autopairs
+      require('mini.pairs').setup()
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
-      statusline.setup()
-
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we disable the section for
-      -- cursor information because line numbers are already enabled
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return ''
-      end
+      require('mini.statusline').setup()
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
