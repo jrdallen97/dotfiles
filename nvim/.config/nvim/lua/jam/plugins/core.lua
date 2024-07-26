0f8f1827e04c8aee58595e7f0ee82591f2765bc6
James Allen <24575899+jrdallen97@users.noreply.github.com>
2024-07-26 15:55:15 +0100

    nvim: Update which-key config, add "buffer local keymaps"

--- nvim/.config/nvim/lua/jam/plugins/core.lua
+++ nvim/.config/nvim/lua/jam/plugins/core.lua
@@ -57,19 +57,28 @@ return {
     -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VeryLazy', -- Sets the loading event to 'VeryLazy'
+    keys = {
+      {
+        '<leader>?',
+        function()
+          require('which-key').show { global = false }
+        end,
+        desc = 'Buffer Local Keymaps (which-key)',
+      },
+    },
     config = function() -- This is the function that runs, AFTER loading
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>e'] = { name = '[E]dit', _ = 'which_key_ignore' },
-        ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = '[H]unk', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>e', group = '[E]dit' },
+        { '<leader>g', group = '[G]it' },
+        { '<leader>h', group = '[H]unk' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>t', group = '[T]oggle' },
+        { '<leader>w', group = '[W]orkspace' },
       }
     end,
   },
