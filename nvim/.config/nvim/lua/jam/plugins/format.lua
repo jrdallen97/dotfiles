9223b5534e38b8c31b7d473c4da681184e955a2c
James Allen <24575899+jrdallen97@users.noreply.github.com>
2024-11-22 11:37:06 +0000

    nvim: add cmd to toggle autoformat

--- nvim/.config/nvim/lua/jam/plugins/format.lua
+++ nvim/.config/nvim/lua/jam/plugins/format.lua
@@ -1,4 +1,4 @@
----Helper to allow stop_after_first behaviour for a subset of formatters
+-- Helper to allow stop_after_first behaviour for a subset of formatters
 ---@param bufnr integer
 ---@param ... string
 ---@return string
@@ -17,16 +17,35 @@ local js = function(bufnr)
   return { 'eslint_d', first(bufnr, 'prettierd', 'prettier') }
 end
 
+-- Add a command to globally enable/disable autoformatting
+vim.api.nvim_create_user_command('FormatToggle', function()
+  if vim.g.disable_autoformat then
+    vim.g.disable_autoformat = false
+    print 'Auto-format enabled'
+  else
+    vim.g.disable_autoformat = true
+    print 'Auto-format disabled'
+  end
+end, { desc = 'Toggle autoformat-on-save' })
+-- Also add a keymap to do it more easily!
+vim.keymap.set('n', '<leader>tf', ':FormatToggle<cr>', { desc = '[T]oggle [F]ormat' })
+
 return {
   {
     -- Autoformat
     'stevearc/conform.nvim',
     opts = {
       notify_on_error = false,
-      format_after_save = {
-        timeout_ms = 5000,
-        lsp_fallback = true,
-      },
+      format_after_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return {
+          timeout_ms = 5000,
+          lsp_format = 'fallback',
+        }
+      end,
       formatters_by_ft = {
         lua = { 'stylua' },
 
