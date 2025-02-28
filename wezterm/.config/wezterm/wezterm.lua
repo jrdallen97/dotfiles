3ed76d04ba559009fd6b4dbfdbd5cfe7d0c75baf
James Allen <24575899+jrdallen97@users.noreply.github.com>
2025-02-28 16:40:29 +0000

    wezterm: just some normal defaults

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,5 +5,12 @@ config.color_scheme = "iTerm2 Default"
 
 config.font = wezterm.font("Hack Nerd Font Mono")
 config.font_size = 14
+config.adjust_window_size_when_changing_font_size = false
+
+config.enable_scroll_bar = true
+config.scrollback_lines = 100000
+
+-- By default wezterm uses a weird fake fullscreen mode
+config.native_macos_fullscreen_mode = true
 
 return config
