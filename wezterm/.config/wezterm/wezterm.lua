d17eceb85dbe540156fa3cb19260c6da839a0baf
James Allen <24575899+jrdallen97@users.noreply.github.com>
2025-03-25 16:16:09 +0000

    wezterm: appearance

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,11 +1,23 @@
-local wezterm = require("wezterm")
+local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.color_scheme = "iTerm2 Default"
+config.color_scheme = 'iTerm2 Default'
 
-config.font = wezterm.font("Hack Nerd Font Mono")
-config.font_size = 14
+-- Icons look better using the built-in fallback nerd font rather than an actual patched nerd font (idk why)
+config.font = wezterm.font 'Hack'
+config.font_size = 15
 config.adjust_window_size_when_changing_font_size = false
+
+-- Reduce padding
+config.window_padding = {
+  left = '0',
+  right = '0',
+  top = '0.5cell',
+  bottom = '0.5cell',
+}
+
+-- Disable weird font features (combining characters, ligatures)
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 
 config.enable_scroll_bar = true
 config.scrollback_lines = 100000
