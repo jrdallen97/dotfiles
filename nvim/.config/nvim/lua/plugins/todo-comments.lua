6d258a05862bbb33201a036935cdddfc997e40bc
James Allen <24575899+jrdallen97@users.noreply.github.com>
2025-06-18 09:34:46 +0100

    nvim: add wrapping to todo-comments bindings

--- nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -30,21 +30,65 @@ return {
       pattern = [[\b(KEYWORDS)(:|\s|$)]],
     },
   },
-  keys = {
-    {
-      ']t',
-      function()
-        -- TODO: it's a shame this doesn't loop
-        require('todo-comments').jump_next()
-      end,
-      desc = 'Next [T]odo comment',
-    },
-    {
-      '[t',
-      function()
-        require('todo-comments').jump_prev()
-      end,
-      desc = 'Previous [T]odo comment',
-    },
-  },
+  init = function()
+    local config = require 'todo-comments.config'
+    local highlight = require 'todo-comments.highlight'
+    local util = require 'todo-comments.util'
+
+    -- Rewrite the entire jump function to add wrapping on end of file
+    ---@param up boolean
+    local function jump(up, opts)
+      opts = opts or {}
+      opts.keywords = opts.keywords or {}
+
+      local win = vim.api.nvim_get_current_win()
+      local buf = vim.api.nvim_get_current_buf()
+
+      local pos = vim.api.nvim_win_get_cursor(win)
+      local line_count = vim.api.nvim_buf_line_count(buf)
+
+      local from = up and pos[1] - 1 or pos[1] + 1
+      local to = up and 1 or line_count
+      if opts.continue then
+        from = up and line_count or 1
+        to = up and pos[1] + 1 or pos[1] - 1
+      end
+
+      for l = from, to, up and -1 or 1 do
+        local line = vim.api.nvim_buf_get_lines(buf, l - 1, l, false)[1] or ''
+        local ok, start, _, kw = pcall(highlight.match, line)
+
+        if ok and start then
+          if config.options.highlight.comments_only and highlight.is_comment(buf, l - 1, start) == false then
+            kw = nil
+          end
+        end
+
+        if kw and #opts.keywords > 0 and not vim.tbl_contains(opts.keywords, kw) then
+          kw = nil
+        end
+
+        if kw then
+          vim.api.nvim_win_set_cursor(win, { l, start - 1 })
+          return
+        end
+      end
+
+      if opts.continue then
+        -- If we get this far after continuing, emit a warning
+        util.warn 'No todo comments in file'
+      else
+        -- Otherwise, try again from the start/end of the file
+        opts.continue = true
+        jump(up, opts)
+      end
+    end
+
+    vim.keymap.set('n', ']t', function()
+      jump(false)
+    end, { desc = 'Next [T]odo comment' })
+    vim.keymap.set('n', '[t', function()
+      jump(true)
+    end, { desc = 'Previous [T]odo comment' })
+  end,
 }
