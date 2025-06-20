2339ab176a6ec13e4b7845be8f02ada94318d723
James Allen <24575899+jrdallen97@users.noreply.github.com>
2025-06-20 19:42:31 +0100

    nvim: tidy up settings (based on mini.basics)

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -1,91 +1,68 @@
--- [[ Setting options ]]
--- See `:help vim.o`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
+-- Get or set options (`:h vim.o`)
+local o = vim.o
+-- Like `vim.o` but with a special interface for interacting with lists & maps (`:h vim.opt`)
+--  See `:help lua-options`
+--  and `:help lua-options-guide`
+local opt = vim.opt
+-- NOTE: For more options, see `:help option-list`
 
--- Make line numbers default
-vim.o.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+-- Disable formatting for basic settings so I can line them up nicely:
+-- stylua: ignore
+do
+  -- General
+  o.undofile   = true -- Enable persistent undo (see also `:h undodir`)
+  o.mouse      = 'a'  -- Enable mouse for all available modes
+  o.updatetime = 100  -- Decrease update time
+  o.timeoutlen = 500  -- Decrease mapped sequence wait time
+  o.confirm    = true -- Ask whether you want to save when running commands like `:q` or `:e`
 
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.o.mouse = 'a'
+  -- Appearance
+  o.number        = true    -- Show line numbers
+  o.cursorline    = true    -- Highlight current line
+  o.showmode      = false   -- Don't show mode in command line
+  o.signcolumn    = 'yes'   -- Always show sign column (otherwise it will shift text)
+  o.scrolloff     = 5       -- Number of visible lines to keep above and below the cursor
+  o.sidescrolloff = 5       -- Number of visible columns to keep left and right of the cursor
+  o.fillchars     = 'eob: ' -- Don't show `~` outside of buffer
+  o.list          = true    -- Show certain hidden characters
+  opt.listchars   = {       -- Make hidden characters display nicely
+    tab = '» ',
+    trail = '·',
+    nbsp = '␣',
+  }
 
--- Don't show the mode, since it's already in the status line
-vim.o.showmode = false
+  -- Wrapping
+  o.wrap        = true -- Enable line wrapping by default
+  o.linebreak   = true -- Wrap long lines at 'breakat'
+  o.breakindent = true -- Indent wrapped lines to match line start
+
+  -- Splits
+  o.splitbelow = true -- Horizontal splits will be below
+  o.splitright = true -- Vertical splits will be to the right
+
+  -- Search/Replace
+  o.ignorecase  = true    -- Ignore case when searching (unless you include `\C` in search)
+  o.incsearch   = true    -- Show search results while typing
+  o.infercase   = true    -- Infer letter cases for a richer built-in keyword completion
+  o.smartcase   = true    -- Don't ignore case when searching if pattern has upper case
+  o.smartindent = true    -- Make indenting smart
+  o.hlsearch    = true    -- Highlight all matches when searching
+  o.inccommand  = 'split' -- Preview substitutions as you type
+
+  -- Folds
+  o.foldtext       = ''     -- Show the first line of the fold as-is
+  o.foldlevelstart = 99     -- Don't fold anything by default
+  o.foldmethod     = 'expr' -- Enable treesitter-based code folding by default
+  o.foldexpr       = 'v:lua.vim.treesitter.foldexpr()'
+end
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
+  o.clipboard = 'unnamedplus'
 end)
-
--- Turn on line wrapping by default
-vim.o.wrap = true
--- Slightly smarter line wrapping
-vim.o.linebreak = true
--- Keep wrapped lines at the same indentation as the first line
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Highlight all matches when searching
-vim.o.hlsearch = true
-
--- Keep signcolumn on by default
-vim.o.signcolumn = 'yes'
-
--- Decrease update time
-vim.o.updatetime = 250
--- Decrease mapped sequence wait time
-vim.o.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.o.splitright = true
-vim.o.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
---
--- Notice listchars is set using `vim.opt` instead of `vim.o`.
--- It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
---  See `:help lua-options`
---  and `:help lua-options-guide`
-vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type!
-vim.o.inccommand = 'split'
-
--- Show which line your cursor is on
-vim.o.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 5
--- Minimal number of screen columns to keep left and right of the cursor.
-vim.o.sidescrolloff = 5
-
--- If performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
-vim.o.confirm = true
-
--- Enable treesitter-based code folding by default
-vim.o.foldmethod = 'expr'
-vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
--- Don't fold anything by default
-vim.o.foldlevelstart = 99
--- Leave the first line of the fold as-is
-vim.o.foldtext = ''
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -97,7 +74,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.hl.on_yank()
+    vim.hl.on_yank { timeout = 250 }
   end,
 })
 
