bf41fac44cec34842761fc0687dd3ca799a29865
James Allen <24575899+jrdallen97@users.noreply.github.com>
2026-01-22 16:40:41 +0000

    zsh: misc tidy up

--- helpers.zsh
+++ helpers.zsh
@@ -1,24 +1,3 @@
-CONFIG_DIR=${XDG_CONFIG_HOME:=$HOME/.config}
-
-link_file () {
-  dotfile="$1"
-  target="$2"
-
-  if [[ ! -e $target || $FORCE -eq 1 ]]; then
-    ln -sfnv "$dotfile" "$target"
-  else
-    echo "$target already exists, skipping..."
-  fi
-}
-
-link_to_home() {
-  link_file "${DOTFILES}/${1}" "${HOME}/${2}"
-}
-
-link_to_config() {
-  link_file "${DOTFILES}/${1}" "${CONFIG_DIR}/${2}"
-}
-
 pull_repo() {
   REPO="$1"
   DIR="$2"
@@ -36,4 +15,3 @@ pull_repo() {
     (cd "$DIR" && git pull)
   fi
 }
-
