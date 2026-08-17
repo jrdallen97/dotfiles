866db1187781461dc676f20e19e44b7120adfdff
James Allen <24575899+jrdallen97@users.noreply.github.com>
2026-08-17 14:41:32 +0100

    nvim: pack: migrate conform

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -67,6 +67,7 @@ vim.pack.add {
   { src = 'https://github.com/nvim-lualine/lualine.nvim' },
   { src = 'https://github.com/nvim-mini/mini.nvim' },
   { src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
+  { src = 'https://github.com/stevearc/conform.nvim' },
   { src = 'https://github.com/tpope/vim-fugitive' },
   { src = 'https://github.com/windwp/nvim-ts-autotag' },
 
---
+++ nvim/.config/nvim/plugin/conform.lua
@@ -0,0 +1,57 @@
+-- Helper to allow stop_after_first behaviour for a subset of formatters
+---@param bufnr integer
+---@param ... string
+---@return string
+local function first(bufnr, ...)
+  local conform = require 'conform'
+  for i = 1, select('#', ...) do
+    local formatter = select(i, ...)
+    if conform.get_formatter_info(formatter, bufnr).available then
+      return formatter
+    end
+  end
+  return select(1, ...)
+end
+
+-- Shorthand for which formatters to use for all JS files
+local function js(bufnr)
+  return { 'eslint_d', first(bufnr, 'oxfmt', 'prettierd', 'prettier') }
+end
+
+-- Autoformat
+require('conform').setup {
+  notify_on_error = false,
+
+  -- Use format_after_save rather than format_on_save to make it async!
+  format_after_save = function(bufnr)
+    -- Disable with a global or buffer-local variable
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      return
+    end
+
+    return {
+      timeout_ms = 5000,
+      lsp_format = 'fallback',
+    }
+  end,
+
+  formatters_by_ft = {
+    lua = { 'stylua' },
+
+    javascript = js,
+    javascriptreact = js,
+    typescript = js,
+    typescriptreact = js,
+    json = { 'fixjson' },
+
+    go = { 'gofmt', 'goimports' },
+  },
+}
+
+-- Command to manually trigger formatting
+vim.api.nvim_create_user_command('Format', function()
+  require('conform').format { async = true, lsp_format = 'fallback' }
+end, { desc = 'Format buffer' })
+
+-- Keybind to manually trigger formatting
+vim.keymap.set('n', '<leader>F', '<cmd>Format<cr>', { desc = 'Format buffer' })
