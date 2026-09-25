6b4226f9cb09f632049f1cc78dd874d63b2d2260
James Allen <24575899+jrdallen97@users.noreply.github.com>
2026-09-25 14:40:05 +0100

    nvim: autosave after running :Mtoc

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -13,5 +13,15 @@ vim.bo.shiftwidth = 4
 vim.bo.expandtab = true
 vim.bo.textwidth = 0
 
+-- Autosave after running :Mtoc
+vim.api.nvim_create_autocmd('CmdlineLeave', {
+  buffer = 0,
+  callback = function()
+    if not vim.v.event.abort and vim.v.event.cmdtype == ':' and vim.fn.getcmdline() == 'Mtoc' then
+      vim.schedule(vim.cmd.write)
+    end
+  end,
+})
+
 -- Disable mini.move's auto-indentation bc it does weird stuff with lists
 vim.b.minimove_config = { options = { reindent_linewise = false } }
